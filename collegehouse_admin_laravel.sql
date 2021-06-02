a3443896fae37af359a9ad07619b27b756d0f12f
pksaudi4 <ahmedalisayed13@gmail.com>
2021-06-02 16:27:30 +0200

    ahmed

--- collegehouse_admin_laravel.sql
+++ collegehouse_admin_laravel.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 02:01 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Jun 02, 2021 at 04:27 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,10 +39,8 @@ CREATE TABLE `cities` (
 --
 
 INSERT INTO `cities` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'city1', NULL, NULL),
-(2, 'city2', NULL, NULL),
-(3, 'city3', NULL, NULL),
-(4, 'city4', NULL, NULL);
+(1, 'option1', NULL, NULL),
+(2, 'option2', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -65,7 +62,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2021_06_01_125359_create_roles_table', 1),
 (2, '2021_06_01_125813_create_cities_table', 2),
 (3, '2014_10_12_000000_create_users_table', 3),
-(4, '2014_10_12_100000_create_password_resets_table', 3);
+(4, '2014_10_12_100000_create_password_resets_table', 4);
 
 -- --------------------------------------------------------
 
@@ -97,9 +94,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '2021-06-01 13:00:34', '2021-06-01 13:00:34'),
-(2, 'owner', '2021-06-01 13:00:34', '2021-06-01 13:00:34'),
-(3, 'tenant', '2021-06-01 13:01:08', '2021-06-01 13:01:08');
+(1, 'admin', NULL, NULL),
+(2, 'owner', NULL, NULL),
+(3, 'tenant', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -130,9 +127,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone`, `address`, `state`, `city_id`, `zip`, `email`, `email_verified_at`, `password`, `role_id`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ahmed', 'ali sayed', '01112912233', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'wedwe', 2, 'ewr3w', 'admin@gmail.com', NULL, '$2y$10$3oWWERiU3W22KsmWAw05Iua.rDfQefUBT8/QK4TyYx2Cs3p0baxI6', 1, NULL, '2021-06-01 12:25:06', '2021-06-01 21:27:26'),
-(2, 'ahmed', 'ali', '01112913344', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'hjthj', 1, 'fdgrf', 'student@gmail.com', NULL, '$2y$10$zR92d6lc1.0mwudhbl4n/uHg5xqT6G6hPtpJSJsAzI8OMSeDWgOO2', 3, NULL, '2021-06-01 12:34:11', '2021-06-01 12:34:11'),
-(3, 'ahmed', 'ali', '01112912244', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'ed23edx', 1, '234e23', 'owner@gmail.com', NULL, '$2y$10$.Kt9BrklKp5xhxa135XpmunfSpvGZ/aUyuL6QJf.YVTvDH7NCIrjS', 2, NULL, '2021-06-01 12:45:45', '2021-06-01 12:45:45');
+(2, 'ahmed', 'ali sayed', '01112912233', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'qwde', 1, 'eqd', 'owner@gmail.com', NULL, '$2y$10$J01pToCEgIkwXq4gwP2V4uwPEz/A5Fh.j0O7Om3Yb0sHyXlJzvHjS', 2, NULL, '2021-06-02 08:42:20', '2021-06-02 09:28:09'),
+(3, 'ahmed', 'mostafa ali', '01112912244', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'rerbd', 2, 'gredger', 'tenant@gmail.com', NULL, '$2y$10$kLx2oWvk52i6K5LSb1f1cOcUnVGvD3rfK0zGiT2Z/U1/eiD7ETGzm', 3, NULL, '2021-06-02 09:35:35', '2021-06-02 09:52:15'),
+(4, 'ahmed', 'nour', '01112912211', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'wefcew', 2, 'ddwfcwe', 'admin@gmail.com', NULL, '$2y$10$wN/OoMc2sempSN8Kz3iFOOLSXz93UyJfkDahudqC39fXTSbWUID3e', 1, NULL, '2021-06-02 09:59:25', '2021-06-02 09:59:25'),
+(5, 'ahmed', 'ali', '01112912999', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'dsds', 1, 'sdcsd', 'owner2@gmail.com', NULL, '$2y$10$FxDqg1eBjmL4p83UhM5A/uHOB0IIXoSmO7hIFc4.OaCDoNUj/z4P2', 2, NULL, '2021-06-02 11:37:23', '2021-06-02 11:37:23');
 
 --
 -- Indexes for dumped tables
@@ -179,7 +177,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -197,7 +195,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
