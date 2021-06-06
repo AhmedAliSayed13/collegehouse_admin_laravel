5906a90bbdff61755f504dc15725c595c24bac58
pksaudi4 <44751885+AhmedAliSayed13@users.noreply.github.com>
2021-06-06 17:18:13 +0200

    ahmed

--- collegehouse_admin_laravel.sql
+++ collegehouse_admin_laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 04:27 PM
+-- Generation Time: Jun 06, 2021 at 05:17 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -39,8 +39,91 @@ CREATE TABLE `cities` (
 --
 
 INSERT INTO `cities` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'option1', NULL, NULL),
-(2, 'option2', NULL, NULL);
+(1, 'option11', NULL, NULL),
+(2, 'option22', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `front_house_images`
+--
+
+CREATE TABLE `front_house_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `house_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `front_house_images`
+--
+
+INSERT INTO `front_house_images` (`id`, `name`, `house_id`, `created_at`, `updated_at`) VALUES
+(1, '16229925160front_house_image.png', 4, '2021-06-06 13:15:16', '2021-06-06 13:15:16'),
+(2, '16229925161front_house_image.png', 4, '2021-06-06 13:15:16', '2021-06-06 13:15:16'),
+(3, '16229925162front_house_image.png', 4, '2021-06-06 13:15:16', '2021-06-06 13:15:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+
+CREATE TABLE `houses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `owner_id` bigint(20) UNSIGNED NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `house_type_id` bigint(20) UNSIGNED NOT NULL,
+  `num_rooms` int(11) NOT NULL,
+  `num_residents` int(11) NOT NULL,
+  `num_bathrooms` int(11) NOT NULL,
+  `num_flooers` int(11) NOT NULL,
+  `num_parkings` int(11) NOT NULL,
+  `total_size` int(11) NOT NULL,
+  `num_kitchens` int(11) NOT NULL,
+  `annual_reset` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payment_method_id` bigint(20) UNSIGNED NOT NULL,
+  `image_ownership` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image_lease` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `houses`
+--
+
+INSERT INTO `houses` (`id`, `owner_id`, `address`, `status`, `city_id`, `name`, `house_type_id`, `num_rooms`, `num_residents`, `num_bathrooms`, `num_flooers`, `num_parkings`, `total_size`, `num_kitchens`, `annual_reset`, `payment_method_id`, `image_ownership`, `image_lease`, `created_at`, `updated_at`) VALUES
+(3, 2, 'التوسعات الشمالية - قطعة 816', 'erew', 1, 'ewew', 2, 12, 12, 12, 12, 12, 121, 12, 'sdfcds', 1, '1622992486image_ownership.png', '1622992486image_lease.png', '2021-06-06 13:14:46', '2021-06-06 13:14:46'),
+(4, 2, 'التوسعات الشمالية - قطعة 816', 'erew', 1, 'ewew', 2, 12, 12, 12, 12, 12, 121, 12, 'sdfcds', 1, '1622992516image_ownership.png', '1622992516image_lease.png', '2021-06-06 13:15:16', '2021-06-06 13:15:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `house_types`
+--
+
+CREATE TABLE `house_types` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `house_types`
+--
+
+INSERT INTO `house_types` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'op1', NULL, NULL),
+(2, 'op2', NULL, NULL),
+(3, 'op3', NULL, NULL),
+(4, 'op4', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -62,7 +145,13 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2021_06_01_125359_create_roles_table', 1),
 (2, '2021_06_01_125813_create_cities_table', 2),
 (3, '2014_10_12_000000_create_users_table', 3),
-(4, '2014_10_12_100000_create_password_resets_table', 4);
+(4, '2014_10_12_100000_create_password_resets_table', 4),
+(5, '2021_06_06_123645_create_house_types_table', 5),
+(6, '2021_06_06_124631_create_payment_methods_table', 6),
+(9, '2021_06_06_123206_create_houses_table', 7),
+(10, '2021_06_06_142233_create_forent_house_images_table', 8),
+(11, '2021_06_06_144121_create_froent_house_images_table', 9),
+(12, '2021_06_06_144450_create_front_house_images_table', 10);
 
 -- --------------------------------------------------------
 
@@ -75,6 +164,29 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods`
+--
+
+CREATE TABLE `payment_methods` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payment_methods`
+--
+
+INSERT INTO `payment_methods` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'op1', NULL, NULL),
+(2, 'op2', NULL, NULL),
+(3, 'op3', NULL, NULL),
+(4, 'op4', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -130,7 +242,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone`, `address`, `state
 (2, 'ahmed', 'ali sayed', '01112912233', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'qwde', 1, 'eqd', 'owner@gmail.com', NULL, '$2y$10$J01pToCEgIkwXq4gwP2V4uwPEz/A5Fh.j0O7Om3Yb0sHyXlJzvHjS', 2, NULL, '2021-06-02 08:42:20', '2021-06-02 09:28:09'),
 (3, 'ahmed', 'mostafa ali', '01112912244', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'rerbd', 2, 'gredger', 'tenant@gmail.com', NULL, '$2y$10$kLx2oWvk52i6K5LSb1f1cOcUnVGvD3rfK0zGiT2Z/U1/eiD7ETGzm', 3, NULL, '2021-06-02 09:35:35', '2021-06-02 09:52:15'),
 (4, 'ahmed', 'nour', '01112912211', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'wefcew', 2, 'ddwfcwe', 'admin@gmail.com', NULL, '$2y$10$wN/OoMc2sempSN8Kz3iFOOLSXz93UyJfkDahudqC39fXTSbWUID3e', 1, NULL, '2021-06-02 09:59:25', '2021-06-02 09:59:25'),
-(5, 'ahmed', 'ali', '01112912999', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'dsds', 1, 'sdcsd', 'owner2@gmail.com', NULL, '$2y$10$FxDqg1eBjmL4p83UhM5A/uHOB0IIXoSmO7hIFc4.OaCDoNUj/z4P2', 2, NULL, '2021-06-02 11:37:23', '2021-06-02 11:37:23');
+(5, 'ahmed', 'ali', '01112912999', '٤٢٩ المحور المركزى _ الحي الأول - 6 أكتوبر _ أمام جامعه 6 اكتوبر', 'dsds', 1, 'sdcsd', 'owner2@gmail.com', NULL, '$2y$10$FxDqg1eBjmL4p83UhM5A/uHOB0IIXoSmO7hIFc4.OaCDoNUj/z4P2', 2, NULL, '2021-06-02 11:37:23', '2021-06-02 11:37:23'),
+(6, 'nour1', 'qwede', '43638248723', 'التوسعات الشمالية - قطعة 816', 'ergerge', 1, 'ergver', 'ahmednfdfour@gmail.com', NULL, '$2y$10$fwU5ATr2Wvir1rUGhTzWrOSOnLsIUJgzIXzQc6S4su8AzGsUTPOk.', 2, NULL, '2021-06-06 11:37:52', '2021-06-06 11:37:52');
 
 --
 -- Indexes for dumped tables
@@ -140,6 +253,29 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone`, `address`, `state
 -- Indexes for table `cities`
 --
 ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `front_house_images`
+--
+ALTER TABLE `front_house_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `front_house_images_house_id_foreign` (`house_id`);
+
+--
+-- Indexes for table `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `houses_owner_id_foreign` (`owner_id`),
+  ADD KEY `houses_city_id_foreign` (`city_id`),
+  ADD KEY `houses_house_type_id_foreign` (`house_type_id`),
+  ADD KEY `houses_payment_method_id_foreign` (`payment_method_id`);
+
+--
+-- Indexes for table `house_types`
+--
+ALTER TABLE `house_types`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -153,6 +289,12 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `roles`
@@ -180,10 +322,34 @@ ALTER TABLE `cities`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `front_house_images`
+--
+ALTER TABLE `front_house_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `house_types`
+--
+ALTER TABLE `house_types`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -195,11 +361,26 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `front_house_images`
+--
+ALTER TABLE `front_house_images`
+  ADD CONSTRAINT `front_house_images_house_id_foreign` FOREIGN KEY (`house_id`) REFERENCES `houses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `houses`
+--
+ALTER TABLE `houses`
+  ADD CONSTRAINT `houses_city_id_foreign` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `houses_house_type_id_foreign` FOREIGN KEY (`house_type_id`) REFERENCES `house_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `houses_owner_id_foreign` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `houses_payment_method_id_foreign` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
