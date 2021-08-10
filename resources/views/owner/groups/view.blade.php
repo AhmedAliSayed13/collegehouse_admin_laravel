46e85b36c47580eae12fb0519cb690e585c797ca
pksaudi4 <44751885+AhmedAliSayed13@users.noreply.github.com>
2021-08-10 17:45:06 +0200

    ggg
    sdgfdf

--- resources/views/owner/groups/view.blade.php
+++ resources/views/owner/groups/view.blade.php
@@ -14,6 +14,81 @@
     </div>
     <!-- /Page Header -->
     <div class="row">
+    <div class="col-lg-12">
+								<div class="card">
+									<div class="card-body">
+
+										<div class="row">
+											<div class="col-lg-12">
+												<div class="card">
+													<div class="card-header">
+														<h4 class="card-title">Meeting information</h4>
+													</div>
+													
+												</div>
+											</div>
+                                            <div class="col-md-12">
+												@include('flash-message')
+												
+												<form method="POST" action="{{route('admin.zoom-edit')}}">
+													{{ csrf_field() }}
+                                                    @method('PATCH')
+													
+													
+													<div class="row">
+														<div class="col-md-12">
+															<div class="form-group">
+																<label>Meeting Date:</label>
+																
+																<input id="meeting_date" type="datetime-local" min="{{Carbon\Carbon::now()->format('Y-m-d\T00:00:00')}}" class="form-control @error('meeting_date') is-invalid @enderror" name="meeting_date" value="{{ old('meeting_date') }}" autocomplete="meeting_date" autofocus>
+																@error('meeting_date')
+																<span class="invalid-feedback" role="alert">
+																	<strong>{{ $message }}</strong>
+																</span>
+																@enderror
+															</div>
+														</div>
+													
+														
+														
+														
+
+														<div class="col-md-12">
+															<div class="form-group">
+																<label>Meeting Status:</label>
+																<select id="meeting_case_id"  class="form-control @error('meeting_case_id') is-invalid @enderror" name="meeting_case_id"  autocomplete="meeting_case_id" autofocus>
+																	<option>Select status </option>
+																
+																</select>
+																	@error('city_id')
+																<span class="invalid-feedback" role="alert">
+																	<strong>{{ $message }}</strong>
+																</span>
+																@enderror
+
+															</div>
+														</div>
+														
+														
+													</div>
+													<button type="submit" class="btn btn-primary" id="id-form1">
+														Update info
+													</button>
+
+												</form>
+											</div>
+
+											
+										</div>
+
+
+
+									</div>
+								</div>
+
+
+
+							</div>
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
